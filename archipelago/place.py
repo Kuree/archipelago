f4224966636adface638ae64d7db5055271ba54e
Keyi Zhang <keyi@stanford.edu>
2019-05-09 20:56:27 -0700

    fix typo

--- archipelago/place.py
+++ archipelago/place.py
@@ -9,8 +9,9 @@ def place(packed_filename: str, layout_filename: str, placement_filename: str,
     placer_binary = os.path.join(path, "placer")
     assert os.path.isfile(placer_binary), placer_binary + " not found"
     if fixed:
-        subprocess.check_call([placer_binary, layout_filename, packed_filename,
+        subprocess.check_call([placer_binary, "-f", layout_filename,
+                               packed_filename,
                                placement_filename])
     else:
-        subprocess.check_call([placer_binary, "-f", layout_filename,
+        subprocess.check_call([placer_binary, layout_filename,
                                packed_filename, placement_filename])
