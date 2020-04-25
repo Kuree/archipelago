28d7ef4ccf36b3ed9e7238c9e660cf98feeaeb38
Keyi Zhang <keyi@stanford.edu>
2020-04-25 11:59:51 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=[
         "archipelago"
     ],
-    version='0.0.5',
+    version='0.0.6',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Fast CGRA PnR based on thunder and cyclone',
